70a53d32b026f9d50330bae0c1110898055f92d4
Leon Tan <natnoel@live.com>
2018-10-19 16:49:33 +0800

    Practical 2 qn 4 changed to private variables by adding double underscores

--- Practical 2/4.py
+++ Practical 2/4.py
@@ -1,45 +1,45 @@
 class BankAccount:
-    acc_no = ''
-    bal = 0
+    __acc_no = ''
+    __bal = 0
 
     def __init__(self, acc_no):
-        self.acc_no = acc_no
-        self.bal = 0
+        self.__acc_no = acc_no
+        self.__bal = 0
 
     def get_acc_no(self):
-        return self.acc_no
+        return self.__acc_no
 
     def get_bal(self):
-        return self.bal
+        return self.__bal
 
     def deposit(self, amt):
-        self.bal += amt
+        self.__bal += amt
 
     def withdraw(self, amt):
-        if self.bal >= amt:
-            self.bal -= amt
+        if self.__bal >= amt:
+            self.__bal -= amt
         else:
             print("You are too poor")
 
     def get_interest_rate(self):
-        if str(self.acc_no).startswith('1'):
+        if str(self.__acc_no).startswith('1'):
             return 0.01
-        elif str(self.acc_no).startswith('0'):
+        elif str(self.__acc_no).startswith('0'):
             return 0.005
         else:
             return 0
 
     def get_interest(self):
-        return self.bal * self.get_interest_rate()
+        return self.__bal * self.get_interest_rate()
 
 
 acc = BankAccount('10-12345-11')
 
 acc.deposit(500)
-print(f"You have", acc.bal)
+print(f"You have", acc.get_bal())
 
 acc.withdraw(100)
-print(f"You are left with", acc.bal)
+print(f"You are left with", acc.get_bal())
 
 print(f"Your interest rate is", acc.get_interest_rate())
 print(f"Your interest earned is", acc.get_interest())
